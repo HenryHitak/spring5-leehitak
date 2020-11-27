c1fb2bd185d643045a12c7c74f3b1a849796f2b2
gmlxk <gmlxk@Henry-Laptop>
2020-11-27 11:50:00 +0900

    20201127 3교시 업데이트

--- src/main/java/org/edu/controller/HomeController.java
+++ src/main/java/org/edu/controller/HomeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 /**
  * 
- * @author gmlxk
+ * @author gmlxkr
  *
  */
 @Controller
@@ -31,7 +31,19 @@ public class HomeController {
 	 * 저장 시, 콘솔에 보시면 reloading context with name ...is completed.재 실행 문자 나옴.
 	 * .java 클래스는 수정 후 약간 기다린 후 Reloading..메시지 후 결과 확인 가능.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	public String index() {//메서드(함수)의 구성은 리턴(반환값 출력)형태지정 메서드명() {구현내용} String index(){}
+		//상단 리퀘스트매핑에서 지정한 결로로 접근을 하게되면, 아래에 있는 index()메서드를 실행
+		//메서드명 제일 앞에있는 public은 접근권한에 대한 키워드, public(공공-다른 클래스에서 겁근가능)
+		//, private(내부-현재 HomeController 클래스에서만 접근가능)
+		// public String index(){}메서드 기본형태
+		// $(document).ready(function){});j쿼리 기본형태
+		//리턴반환값으로 사용한 String 클래스는 문자열을 반환하고, 여기서는 index.jsp와 매칭되는 문자열을 반환.
+		//스프링 MVC 프로젝트에서 상대 경로의 최상위는 views폴더 최상위 폴더 입니다.
+		//접두사(prefix), 접미사(suffix) index(접두사-파일명) .jsp(접미사-확장자)
+		return "sample/index";//접두사만 반환값으로 넣습니다.
+	}
+	/*@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("개발자들이 변수값 확인용으로 사용하는 것이 로그 입니다. 현재 여러분 컴퓨터 언어는 {} 입니다.", locale);
 		logger.info("환영합니다. 현재 여러분의 컴퓨터 언어는 "+ locale + " 입니다..");
@@ -48,6 +60,6 @@ public class HomeController {
 		//위 model 이라는 클래스형 변수를 이용해서, serverTime 변수값을 아래  home(생략.jsp)로 전송해 줌.		
 		return "home";//결과적으로 return 출력값이 home.jsp에 연동됩니다.
 		//스프링에서 뷰단으로 데이터 이동(전송)에 대해서 살펴 보았습니다.
-	}
+	}*/
 	
 }
